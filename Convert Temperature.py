82c048c39566ccab98719ea1d14697920b094983
WRDelmanto <67394462+WRDelmanto@users.noreply.github.com>
2021-12-29 19:30:56 -0300

    Create Convert Temperature.py

---
+++ Convert Temperature.py
@@ -0,0 +1,14 @@
+def celsiusToFahrenheit(celsius):
+    fahrenheit = (celsius*1.8)+32
+
+    print(fahrenheit)
+
+
+def fahrenheitToCelsius(fahrenheit):
+    celsius = (fahrenheit-32)/1.8
+
+    print(celsius)
+
+
+celsiusToFahrenheit(40.15)
+fahrenheitToCelsius(104.27)
