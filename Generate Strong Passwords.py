f93d1a8a6a0d2b77dc652bdbdcaf702882cab306
WRDelmanto <67394462+WRDelmanto@users.noreply.github.com>
2022-01-02 14:28:54 -0300

    Update Generate Strong Passwords.py

--- Generate Strong Passwords.py
+++ Generate Strong Passwords.py
@@ -1,6 +1,11 @@
 import random
 
-allowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%^&*()_+"
+lowerCase = "abcdefghijklmnopqrstuvwxyz"
+upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "123456789"
+specialCharacters = "123456789!@#$%^&*()_+"
+
+allowedCharacters = lowerCase + upperCase + numbers + specialCharacters
 numberOfCharacters = 16
 
 p = "".join(random.sample(allowedCharacters, numberOfCharacters))
