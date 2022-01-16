0ff2b19490fbcc812ab004652dcb12cf0fd19d38
WRDelmanto <67394462+WRDelmanto@users.noreply.github.com>
2022-01-16 19:28:09 -0300

    Create Send Email.py
    Not Tested

---
+++ Send Email.py
@@ -0,0 +1,23 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import json
+import smtplib
+
+msg = MIMEMultipart()
+
+msg['From'] = "me@gmail.com"
+password = "1234"
+
+msg['To'] = "Someone"
+
+msg['Subject'] = "This is the Subject"
+message = "This is a test"
+
+port = 587
+
+msg.attach(MIMEText(message, "plain"))
+server = smtplib.SMTP("smtp.gmail.com", port=port)
+server.starttls()
+server.login(msg['From'], password)
+server.sendmail(msg['From'], msg['To'], msg.as_string())
+server.quit()
