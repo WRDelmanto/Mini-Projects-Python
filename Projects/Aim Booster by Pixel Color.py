45a5f881c9d385caa0b1137398e53549bfb0f978
WRDelmanto <67394462+WRDelmanto@users.noreply.github.com>
2023-05-09 00:40:31 -0300

    Update Aim Booster by Pixel Color.py

--- Projects/Aim Booster by Pixel Color.py
+++ Projects/Aim Booster by Pixel Color.py
@@ -1,31 +1,56 @@
-from pyautogui import *
-import pyautogui
 import time
+
 import keyboard
-import random
-import win32api, win32con
+import pyautogui
+import win32api
+import win32con
 
-time.sleep(2)
 
-def click(x,y):
-    win32api.SetCursorPos((x,y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+def clickEvent(x, y):
+    # Set the cursor position to the given X and Y coordinates
+    win32api.SetCursorPos((x, y))
 
-#Color of center: (255, 219, 195)
+    # Simulate pressing down the left mouse button
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
 
-while keyboard.is_pressed('q') == False:
+    # Wait for a short amount of time (Prevent hold event)
+    time.sleep(0.01)
 
-    pic = pyautogui.screenshot(region=(660,350,600,400))
+    # Simulate releasing the left mouse button
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
-    width, height = pic.size
 
-    for x in range(0,width,5):
-        for y in range(0,height,5):
+def playAimBooster(stop_key, region, pixel_color_red, pixel_color_green, pixel_color_blue):
+    # Print a start sentence
+    print("Lets Play Aim Booster!")
 
-            r,g,b = pic.getpixel((x,y))
+    # Keep playing the game until the stop key is pressed
+    while keyboard.is_pressed(stop_key) == False:
+        # Take a screenshot of the specified region
+        screenshot = pyautogui.screenshot(region=region)
 
-            if b == 195:
-                click(x+660,y+350)
-                time.sleep(0.05)
-                break
+        # Get the width and height of the screenshot
+        width, height = screenshot.size
+
+        # Loop through every 5 pixels in the screenshot
+        for x in range(0, width, 5):
+            for y in range(0, height, 5):
+
+                # Get the RGB values of the pixel at (x, y)
+                red, green, blue = screenshot.getpixel((x, y))
+
+                # Checks if the pixel matches the specified color, click on it and print a message
+                if red == pixel_color_red and green == pixel_color_green and blue == pixel_color_blue:
+                    # Click on the pixel
+                    clickEvent(x + region[0], y + region[1])
+
+                    # Print when clicked
+                    print("Clicked at ", x, ":", y)
+
+                    # Exit the inner loop and continue with the outer loop
+                    break
+
+
+# http://www.aimbooster.com/
+# Recomended: "Generate Screenshot.py" to help set the coordinates
+playAimBooster("q", (660, 370, 600, 420), 255, 219, 195)
